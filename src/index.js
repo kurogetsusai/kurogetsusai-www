1ba5d4c35444023362d9562a0a9e6a298db881a0
Sai Kurogetsu <kurogetsusai@gmail.com>
2017-09-04 21:25:31 +0000

    add hello world

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './index.xhtml';
+import './index.sass';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './comp/app.jsx';
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
